a3dd3c08e393427fcc468964551dd47a5eaa6b88
Dalton McMullen <thecodingegg@gmail.com>
2025-11-25 21:17:47 -0800

    Set up the AOC2024
    Time to truly learn Rust!

---
+++ src/bin/01.rs
@@ -0,0 +1,26 @@
+advent_of_code::solution!(1);
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let test_input = "3 4\n4 3\n2 5\n1 3\n3 9\n3 3";
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
